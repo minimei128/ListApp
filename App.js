6ccc6268d4c51be80d90653f9ef75fd4e22e56f9
minimei128 <mphan2868@hotmail.com8>
2020-08-03 18:37:37 +1000

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {Item} from './components/Item';
+
+export default class App extends Component {
+
+  //returns some of sort of view/item components
+  render(){
+    return (
+      <SafeAreaView>
+
+      </SafeAreaView>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  main: {
+
+    paddingHorizontal: 10,
+  }
+})
