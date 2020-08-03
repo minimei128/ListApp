ebb39fd3c9b4d0fb2d4e5b3096d7957f90de70ab
minimei128 <mphan2868@hotmail.com8>
2020-08-03 19:51:45 +1000

    added more elements to do list

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button } from 'react-native';
 
 import {Item} from './components/Item';
 
 export default class App extends Component {
+
+  state = {
+    expenseAmount : 0,
+    expenseCategory: ''
+  }
 
   listData = [
     { id: '1', amount:50, category: 'food'},
@@ -16,11 +21,27 @@ export default class App extends Component {
   render(){
     return (
       <SafeAreaView>
+        <Text>Add your expense</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder = "$ amount"
+          onChangeText = { text => this.setState(
+            {expenseAmount: parseFloat(text)})}
+          keyboardType = "number-pad"/>
+        <TextInput
+          style={styles.input}
+          placeholder="category"
+          onChangeText={ text => this.setState(
+            {expenseCategory: text})}/>
+        <Button title = "Add" onPress={this.addItem()}/>
           <FlatList
               data={this.listData}
               renderItem={this.renderList}
               keyExtractor={expense => expense.id}
+              extraData={this.state.expenseCategory}
               />
+        
+
       </SafeAreaView>
     )
   }
@@ -28,12 +49,33 @@ export default class App extends Component {
   renderList = ({item}) => (
     <Item amount = {item.amount} category={item.category} />
   )
-
+addItem = () => {
+  if( isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0){
+    return;
+  }
+  //generate new id
+  let itemId = new Date().getTime().toString()
+  let item = {
+    id: itemId,
+    amount: this.state.expenseAmount,
+    category: this.state.expenseCategory
+  }
+  this.listData.push(item)
+  //trigger our flatlist to render
+  this.setState({expenseAmount:0})
+  }
 }
 
 const styles = StyleSheet.create({
-  main: {
+  main: {   
 
     paddingHorizontal: 10,
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 15
   }
 })
